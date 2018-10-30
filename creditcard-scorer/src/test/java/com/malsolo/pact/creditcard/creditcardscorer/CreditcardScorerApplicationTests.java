6716a4adffd319a1104ad109d2e80da37982898a
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-10-30 08:56:00 +0100

    pact-test: provider app, creditcard-scorer.

---
+++ creditcard-scorer/src/test/java/com/malsolo/pact/creditcard/creditcardscorer/CreditcardScorerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malsolo.pact.creditcard.creditcardscorer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditcardScorerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
