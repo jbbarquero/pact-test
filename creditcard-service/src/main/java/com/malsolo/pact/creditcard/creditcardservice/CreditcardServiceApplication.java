6261773882856438c021cf50f25ea72fe6215230
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-10-30 08:50:30 +0100

    pact-test: consumer app, creditcard-service.

---
+++ creditcard-service/src/main/java/com/malsolo/pact/creditcard/creditcardservice/CreditcardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.pact.creditcard.creditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardServiceApplication.class, args);
+	}
+}
