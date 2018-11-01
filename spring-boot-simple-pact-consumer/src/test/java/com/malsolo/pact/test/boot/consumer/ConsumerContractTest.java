d450f76ae513e5a3e09ea6ae898860950b9d83fd
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-11-01 20:44:58 +0100

    pact-test: spring boot simple pact consumer test, TOTALLY WORKS! Also with Spring Boot test.

--- spring-boot-simple-pact-consumer/src/test/java/com/malsolo/pact/test/boot/consumer/ConsumerContractTest.java
+++ spring-boot-simple-pact-consumer/src/test/java/com/malsolo/pact/test/boot/consumer/ConsumerContractTest.java
@@ -7,6 +7,12 @@ import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.RequestResponsePact;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
@@ -16,12 +22,19 @@ import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConsumerContractTest {
 
-    //@Autowired
-    private ConsumerClient consumerClient = new ConsumerClient(new RestTemplate());
-    //@Value("${producer.url}")
-    private String producerUrl = "http://localhost:9191/producer";
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ConsumerClient consumerClient;// = new ConsumerClient(new RestTemplate());
+    @Value("${producer.url}")
+    private String producerUrl;// = "http://localhost:9191/producer";
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Rule
     public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("boot_simple_pact_producer", "localhost", 9191, this);
@@ -47,7 +60,17 @@ public class ConsumerContractTest {
 
     @Test
     @PactVerification("boot_simple_pact_producer")
-    public void runTest() {
+    public void shouldResponseTheProducerApplication() {
+
+        //To verify that Spring boot test is working
+
+        String consumerUrl = String.format("http://localhost:%d/actuator/info", port);
+        System.out.printf("***** Verify that Spring Boot test is working with local server at %s\n", consumerUrl);
+        String string = restTemplate.getForObject(consumerUrl, String.class);
+        assertThat(string).isNotNull();
+        assertThat(string).isEqualTo("{}");
+
+        //Actual contract test
         Produced produced = consumerClient.consumerProducer(producerUrl,
                 LocalDateTime.now(ZoneOffset.UTC));
         assertThat(produced).isNotNull();
