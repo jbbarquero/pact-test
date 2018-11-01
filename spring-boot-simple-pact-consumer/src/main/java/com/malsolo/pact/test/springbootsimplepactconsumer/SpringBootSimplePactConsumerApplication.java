9c0846756e6600964a2aeb379b1f69f57e2662fc
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-11-01 11:50:26 +0100

    pact-test: spring boot simple pact consumer.

---
+++ spring-boot-simple-pact-consumer/src/main/java/com/malsolo/pact/test/springbootsimplepactconsumer/SpringBootSimplePactConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.pact.test.springbootsimplepactconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimplePactConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimplePactConsumerApplication.class, args);
+	}
+}
---
+++ spring-boot-simple-pact-consumer/src/test/java/com/malsolo/pact/test/springbootsimplepactconsumer/SpringBootSimplePactConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malsolo.pact.test.springbootsimplepactconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSimplePactConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
