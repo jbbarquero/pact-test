d36e65e0828d3788f10c40fe339eb38dff60d138
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-11-02 23:52:33 +0100

    pact-test: spring boot simple pact producer.

---
+++ spring-boot-simple-pact-producer/src/main/java/com/malsolo/pact/test/springbootsimplepactproducer/SpringBootSimplePactProducerApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.pact.test.springbootsimplepactproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimplePactProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimplePactProducerApplication.class, args);
+	}
+}
